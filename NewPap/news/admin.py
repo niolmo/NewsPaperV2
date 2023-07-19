9707d41cceca27a3d7e674685cda7230bee29cc1
niolmo <71871013+niolmo@users.noreply.github.com>
2023-07-19 18:49:08 +0500

    admin start

--- NewPap/news/admin.py
+++ NewPap/news/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Author)
+class AuAam(admin.ModelAdmin):
+    list_display = ['author', 'auRunk']
+    raw_id_fields = ['author']
+
+
+admin.site.register(Category)
+
+
+@admin.register(Post)
+class PostAdm(admin.ModelAdmin):
+    list_display = ['publ', 'sort', 'title', 'slug', 'author']
+    list_filter = ['publ', 'title', 'author']
+    filter_horizontal = ['categories']
+    raw_id_fields = ['author']
+
+
+admin.site.register(Comments)
