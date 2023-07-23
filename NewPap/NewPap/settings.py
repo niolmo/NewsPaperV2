548990fa17d828c3ee56c0cb5dac750bf9f169c3
niolmo <71871013+niolmo@users.noreply.github.com>
2023-07-23 12:39:42 +0500

    tamplates

--- NewPap/NewPap/settings.py
+++ NewPap/NewPap/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIFILES_DIRS = [STATIC_DIR]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
