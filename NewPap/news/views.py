ae82935207e44df8387e1f5b0dfa68d653aa5eed
niolmo <71871013+niolmo@users.noreply.github.com>
2023-07-23 13:47:23 +0500

    templ2

--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
+
+
+class PostView(View):
+    def get(serf, request):
+        posts = Post.objects.all()
+        return render(request, 'templates/news.html', {'post_list': posts})
