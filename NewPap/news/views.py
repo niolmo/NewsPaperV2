8f4b3110d2ae5c3f0aa19026dda739fb83a7bb50
niolmo <71871013+niolmo@users.noreply.github.com>
2023-07-28 11:40:49 +0500

    pagin start

--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Post
+from django.core.paginator import Paginator
 
 
 class PostView(View):
     def get(serf, request):
-        posts = Post.objects.all()
+        post_list = Post.objects.all()
+        paginator = Paginator(post_list, 3)
+        page_nuber = request.GET.get('page', 1)
+        posts = paginator.page(page_nuber)
         return render(request, 'news.html', {'post_list': posts})
