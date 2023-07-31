59bf77bc9a9726bb56c7cc86940e5058aea77246
niolmo <71871013+niolmo@users.noreply.github.com>
2023-07-31 18:43:24 +0500

    Seach conti

--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -15,8 +15,13 @@ class PostView(View):
 
 class Search(View):
     def get(serf, request):
-        post_list = Post.objects.all()
-        paginator = Paginator(post_list, 3)
+        search_list = Post.objects.filter(title=request.GET.get('q'))
+        paginator = Paginator(search_list, 3)
         page_nuber = request.GET.get('page', 1)
         posts = paginator.page(page_nuber)
-        return render(request, 'search.html', {'post_list': posts})
+        return render(request, 'search.html', {'search_list': posts})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["q"] = self.request.GET.get('q')
+        return context
