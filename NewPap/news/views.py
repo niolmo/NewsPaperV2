dd565ae5ea9dbece3644f098e5e6a3546f3ed47a
niolmo <71871013+niolmo@users.noreply.github.com>
2023-08-18 11:24:21 +0500

    rebild

--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.views.generic import ListView
 from .models import Post
 from django.core.paginator import Paginator
 
@@ -13,22 +14,5 @@ class PostView(View):
         return render(request, 'news.html', {'post_list': posts})
 
 
-class Search(View):
-    def get(serf, request):
-        search_list = Post.objects.filter(publ=request.GET.get('q')).filter(
-            title=request.GET.get('d')).filter(author__username=request.GET.get('a'))
-        paginator = Paginator(search_list, 3)
-        page_nuber = request.GET.get('page', 1)
-        posts = paginator.page(page_nuber)
-        return render(request, 'search.html', {'search_list': posts})
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["q"] = self.request.GET.get('q')
-        context["d"] = self.request.GET.get('d')
-        context["a"] = self.request.GET.get('a')
-        return context
-
-
-class AddNews(View):
+class Search(ListView):
     pass
