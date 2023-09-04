f6f2b3e77529d09be9990a16337ee8108ca5909e
niolmo <71871013+niolmo@users.noreply.github.com>
2023-09-04 18:58:06 +0500

    add site

--- NewPap/NewPap/settings.py
+++ NewPap/NewPap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
     'accounts.apps.AccountsConfig',
