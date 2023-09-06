c03ce51fc4358f8f70b24f17a74f9f34fa173777
niolmo <71871013+niolmo@users.noreply.github.com>
2023-09-06 11:10:51 +0500

    regform

--- NewPap/sign/urls.py
+++ NewPap/sign/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginFomView
+from .views import LoginFomView, RegView
 
 app_name = 'Login'
 
 urlpatterns = [
     path('login', LoginFomView.as_view(), name="Login"),
+    path('signup/', RegView.as_view(), name='signup'),
+
 ]
--- NewPap/sign/views.py
+++ NewPap/sign/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .forms import RegForm
 
 
 class LoginFomView(LoginView):
     template_name = 'userlogin.html'
     form_class = AuthenticationForm
+
+
+class NewsView(LoginRequiredMixin, TemplateView):
+    template_name = 'news.html'
+
+
+class RegView(CreateView):
+    model = User
+    form_class = RegForm
+    template_name = 'reg.html'
+    success_url = '/'
