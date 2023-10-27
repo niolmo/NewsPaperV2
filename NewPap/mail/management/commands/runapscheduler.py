efe6a63ffa5eb7567fed7af3ff3995c26cbc7c92
niolmo <71871013+niolmo@users.noreply.github.com>
2023-10-27 15:55:42 +0500

    fin2

---
+++ NewPap/mail/management/commands/runapscheduler.py
@@ -0,0 +1,12 @@
+import logging
+
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+
+logger = logging.getLogger(__name__)
